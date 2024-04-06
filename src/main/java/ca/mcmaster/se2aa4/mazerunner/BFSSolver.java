85b4e6876ed65d7235cd5535017014ee564ce9fd
nasrallr <nasrallr@mcmaster.ca>
2024-04-06 19:12:16 -0400

    Change BFSSolver to use graph representation of maze (close #9)

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/BFSSolver.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/BFSSolver.java
@@ -1,103 +1,70 @@
 package ca.mcmaster.se2aa4.mazerunner;
 
-import java.util.HashSet;
+import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.ArrayList;
+import java.util.Map;
 import java.util.Queue;
-import java.util.Set;
 
 public class BFSSolver implements MazeSolver {
 
-    private Queue<Node> queue = new LinkedList<>();
-    private Set<Position> visitedNodes = new HashSet<>();
-    private Maze maze;
+    private GraphBuilder graphBuilder = new GraphBuilder();
 
     @Override
-    public Path solve(Maze inputMaze) {
-        this.maze = inputMaze;
-        Direction dir = Direction.RIGHT;
-        Position pos = maze.getStart();
-        queue.add(new Node(pos, dir, new Path()));
-        visitedNodes.add(maze.getStart());
-    
-        while (!queue.isEmpty()) {
-            Node currentNode = queue.poll();
-            Path currentPath = currentNode.path;
-            Position currentPosition = currentNode.position;
-            if (currentPosition.equals(maze.getEnd())) {
-                return currentPath;
-            } else {
-                enqueue(currentNode, 'F');
-                enqueue(currentNode, 'R', 'F');
-                enqueue(currentNode, 'L', 'F');
-            }
-    
-            
-        }
-        return new Path(); 
-    }
-    
-
-    private void enqueue(Node currentNode, Character... moves) {
-        Path potentialPath = new Path();
-        for (Character move : moves) {
-            potentialPath.addStep(move);
-        }
-        Node updatedNode = updateNode(currentNode, potentialPath);
-        if (updatedNode != null) {
-            Position nextPosition = updatedNode.position;
-            if (positionIsValid(nextPosition) && visitedNodes.add(nextPosition)) {
-                queue.add(updatedNode);
-            }
-        }
-    }
-
-
-    private Node updateNode(Node currentNode, Path addedPath) {
-        Position position = new Position(currentNode.position.x(), currentNode.position.y());
-        Direction dir = currentNode.direction;
-        Path nodePath = new Path(currentNode.path.getCanonicalForm()); 
-        Path path = addedPath;
+    public Path solve(Maze maze) {
         
-    
-        for (char move : path.getPathSteps()) {
-            nodePath.addStep(move); 
-            switch (move) {
-                case 'F':
-                    position = position.move(dir);
-                    if (!positionIsValid(position)) {
-                        return null;
+        graphBuilder.buildGraph(maze);
+        Node startNode = graphBuilder.getStartNode();
+        Node endNode = graphBuilder.getEndNode();
+
+        Queue<Node> queue = new LinkedList<>();
+        Map<Node, Node> shortestPath = new HashMap<>();
+
+        queue.add(startNode);
+        shortestPath.put(startNode, null);
+
+        while (!queue.isEmpty()) {
+            Node current = queue.poll();
+
+            if (current.equals(endNode)) {
+                return nodeToPath(shortestPath);
+            }
+
+            for (Edge edge : current.getEdges()) {
+                Node neighbor = edge.getEndNode();
+                if (!shortestPath.containsKey(neighbor)) {
+                    shortestPath.put(neighbor, current);
+                    queue.add(neighbor);
+                }
+            }
+        }
+        return new Path();
+    }
+
+    private Path nodeToPath(Map<Node, Node> shortestPathInput) {
+        ArrayList<Node> shortestPath = new ArrayList<>();
+        Node endNode = graphBuilder.getEndNode();
+        while (endNode != null) {
+            shortestPath.add(0, endNode);
+            endNode = shortestPathInput.get(endNode);
+        }
+
+        Path path = new Path();
+        for (int i = 0; i < shortestPath.size() - 1; i++) {
+            Node currentNode = shortestPath.get(i);
+            Node nextNode = shortestPath.get(i + 1);
+
+            for (Edge edge : currentNode.getEdges()) {
+                if (edge.getEndNode().equals(nextNode)) {
+                    for (char c : edge.getPath().getPathSteps()) {
+                        path.addStep(c);
                     }
                     break;
-                case 'R':
-                    dir = dir.turnRight();
-                    break;
-                case 'L':
-                    dir = dir.turnLeft();
-                    break;
+                }
             }
         }
-    
-        return new Node(position, dir, nodePath); 
-    }
-    
 
-    private boolean positionIsValid(Position position) {
-        return position != null && position.x() >= 0 && position.x() < maze.getSizeX()
-                && position.y() >= 0 && position.y() < maze.getSizeY() && !maze.isWall(position);
+        return path;
     }
-
-    @Override
-    public String executionTime(Maze maze) {
-        long start = System.currentTimeMillis();
-        solve(maze);
-        long end = System.currentTimeMillis();
-        double timeInMillis = end - start; // Time in milliseconds
-        return String.format("%.2f", timeInMillis);
-    }
-
-    public Queue<Node> getQueue() {
-        return queue;
-    }
-
     
 }
